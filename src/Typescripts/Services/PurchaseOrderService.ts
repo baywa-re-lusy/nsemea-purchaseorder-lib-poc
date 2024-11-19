32e692a1eda27b72150237d37157014ac9f9c3f8
dariodds <dario.dasilva.private@gmail.com>
2024-11-19 10:34:26 +0100

    create service providing function to set project value on line level

--- src/Typescripts/Services/PurchaseOrderService.ts
+++ src/Typescripts/Services/PurchaseOrderService.ts
@@ -1,1 +1,18 @@
-export class PurchaseOrderService {}
+import {PurchaseOrder} from "../Entities/PurchaseOrder";
+import * as _ from 'lodash';
+import * as log from 'N/log';
+
+export class PurchaseOrderService {
+
+  setProjectOnItemLine (po: PurchaseOrder) {
+
+    log.debug('PurchaseOrderService : setProjectOnItemLine', `Project: ${po.custbody_related_project}`);
+    const project = po.custbody_related_project;
+
+    _.map(po.item, (item) => {
+      log.debug('PurchaseOrderService : setProjectOnItemLine', `Line Project from: ${item.custcol_related_project} to ${project}`);
+      item.custcol_related_project = project;
+    });
+  }
+
+}
