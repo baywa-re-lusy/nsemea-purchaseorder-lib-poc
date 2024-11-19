ec59ac60f07a07f3502d302c9e869c52fe3ed854
dariodds <dario.dasilva.private@gmail.com>
2024-11-19 12:30:45 +0100

    logs to find execution error 4

--- src/Typescripts/Services/PurchaseOrderService.ts
+++ src/Typescripts/Services/PurchaseOrderService.ts
@@ -9,17 +9,17 @@ export class PurchaseOrderService {
     log.debug('PurchaseOrderService : setProjectOnItemLine', `Project: ${po.custbody_related_project}`);
     const project = po.custbody_related_project;
 
-
-
     Object.entries(po.item).map(([key, item], index) => {
 
-      log.debug('PurchaseOrderService : setProjectOnItemLine', `Type of item: ${typeof item}`);
-      log.debug('PurchaseOrderService : setProjectOnItemLine', `Type of item: ${item instanceof PurchaseOrderItemSublist}`);
-      log.debug('PurchaseOrderService : setProjectOnItemLine', `Index: ${index}`);
-      log.debug('PurchaseOrderService : setProjectOnItemLine', `Key: ${key}`);
-      log.debug('PurchaseOrderService : setProjectOnItemLine', `Item: ${item}`);
-      log.debug('PurchaseOrderService : setProjectOnItemLine', `before: ${item.custcol_related_project} -> after: ${project}`);
-      item.custcol_related_project = project;
+      if (item instanceof PurchaseOrderItemSublist) {
+        log.debug('PurchaseOrderService : setProjectOnItemLine', `Type of item: ${typeof item}`);
+        log.debug('PurchaseOrderService : setProjectOnItemLine', `Type of item: ${item instanceof PurchaseOrderItemSublist}`);
+        log.debug('PurchaseOrderService : setProjectOnItemLine', `Index: ${index}`);
+        log.debug('PurchaseOrderService : setProjectOnItemLine', `Key: ${key}`);
+        log.debug('PurchaseOrderService : setProjectOnItemLine', `Item: ${item}`);
+        log.debug('PurchaseOrderService : setProjectOnItemLine', `before: ${item.custcol_related_project} -> after: ${project}`);
+        item.custcol_related_project = project;
+      }
 
     });
   }
