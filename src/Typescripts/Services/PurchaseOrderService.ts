3f7cebddb5c223be6580f5dc01da3a5a0a8cc948
dariodds <dario.dasilva.private@gmail.com>
2024-11-19 12:26:19 +0100

    logs to find execution error 3

--- src/Typescripts/Services/PurchaseOrderService.ts
+++ src/Typescripts/Services/PurchaseOrderService.ts
@@ -1,5 +1,6 @@
 import {PurchaseOrder} from "../Entities/PurchaseOrder";
 import * as log from 'N/log';
+import {PurchaseOrderItemSublist} from "../Entities/PurchaseOrderItemSublist";
 
 export class PurchaseOrderService {
 
@@ -13,11 +14,13 @@ export class PurchaseOrderService {
     Object.entries(po.item).map(([key, item], index) => {
 
       log.debug('PurchaseOrderService : setProjectOnItemLine', `Type of item: ${typeof item}`);
+      log.debug('PurchaseOrderService : setProjectOnItemLine', `Type of item: ${item instanceof PurchaseOrderItemSublist}`);
       log.debug('PurchaseOrderService : setProjectOnItemLine', `Index: ${index}`);
       log.debug('PurchaseOrderService : setProjectOnItemLine', `Key: ${key}`);
       log.debug('PurchaseOrderService : setProjectOnItemLine', `Item: ${item}`);
       log.debug('PurchaseOrderService : setProjectOnItemLine', `before: ${item.custcol_related_project} -> after: ${project}`);
       item.custcol_related_project = project;
+
     });
   }
 
