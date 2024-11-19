3d6b252fecaaacd499be2ce311cfd6a9cfb0a1fc
dariodds <dario.dasilva.private@gmail.com>
2024-11-19 15:40:37 +0100

    array of entries in sublist

--- src/Typescripts/Services/PurchaseOrderService.ts
+++ src/Typescripts/Services/PurchaseOrderService.ts
@@ -10,23 +10,16 @@ export class PurchaseOrderService {
     const project = po.custbody_related_project;
 
     log.debug('PurchaseOrderService : setProjectOnItemLine', `Length: ${po.item.length}`);
-    // const nbrOfLines = po.item.length;
-    //
-    // for (let i = 0; i < nbrOfLines; i++) {
-    //   log.debug('PurchaseOrderService : setProjectOnItemLine', `Line: ${i}`);
-    //   log.debug('PurchaseOrderService : setProjectOnItemLine', `before: ${po.item[i].custcol_related_project} -> after: ${project}`);
-    //   po.item[i].custcol_related_project = project;
-    // }
 
-    po.item._entries.map((item) => {
+    po.item.entries.forEach((item) => {
       log.debug('PurchaseOrderService : setProjectOnItemLine', `before: ${item.custcol_related_project} -> after: ${project}`);
+      item.custcol_related_project = project;
     });
 
     Object.entries(po.item).forEach(([key, item], index) => {
       log.debug('PurchaseOrderService : setProjectOnItemLine', `Key: ${key}`);
       log.debug('PurchaseOrderService : setProjectOnItemLine', `Item: ${item}`);
       log.debug('PurchaseOrderService : setProjectOnItemLine', `before: ${item.custcol_related_project} -> after: ${project}`)
-      item.custcol_related_project = project;
     });
   }
 
